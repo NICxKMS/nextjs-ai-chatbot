136f58dbca8cae109f8eda53c2fb6512dd0ecadf
Nikhil Kumar <121129363+NICxKMS@users.noreply.github.com>
2025-12-27 20:33:00 +0530

    prune md files move ap to archive/oldapp

--- features/sidebar/components/index.ts
+++
@@ -1,9 +0,0 @@
-export { AppSidebar, type AppSidebarProps } from "./app-sidebar";
-export { SidebarHistory, type SidebarHistoryProps } from "./sidebar-history";
-export {
-    SidebarHistoryItem,
-    type SidebarHistoryItemProps,
-    type UpdateVisibilityAction,
-} from "./sidebar-history-item";
-export { SidebarToggle } from "./sidebar-toggle";
-export { SidebarUserNav, type SidebarUserNavProps } from "./sidebar-user-nav";
